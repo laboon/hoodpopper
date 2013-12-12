d2e48f820e19d5bbcbffdd681892263cae07e738
laboon <laboon@gmail.com>
2013-12-12 06:24:14 -0500

    Refactored hoodpop_controller to handle syntax errors in compilation

--- app/controllers/hoodpop_controller.rb
+++ app/controllers/hoodpop_controller.rb
@@ -1,31 +1,30 @@
 class HoodpopController < ApplicationController
   require 'ripper'
 
+  # Make a node out of precursor dashes and actual element
   def nodeify(pre, elem)
     elem = "NIL" if elem.nil?
      "#{pre}#{elem}"
   end
 
+  # Turn a nested array into a printed tree
   def treeify(arr, pre)
     arr.map { |elem| (elem.is_a? Array) ? treeify(elem, pre + "-") : nodeify(pre, elem) }
   end
 
-  def add_html_crs(str)
-    str.gsub(/\n/, "<br>")
-  end
-
+  # Turn an array into a string, with line breaks between
   def stringify(arr)
-     to_return = ""
-     arr.each do |elem|
-       to_return += elem.to_s + "<br>"
-     end
-     to_return
+    arr.map { |s| s.to_s }.join("<br>")
    end
 
+  # Turn a string with line breaks into an array separated by said
+  # line breaks
   def arrayify(str)
     str.split("\n")
   end
 
+  # Main method of controller - set @op (operation) and @code
+  # (displayed code) variables for corresponding view to use
   def index
     @op = params[:commit]
     orig_code = params[:code][:code].to_s
@@ -35,7 +34,11 @@ class HoodpopController < ApplicationController
     when 'Parse'
       @code = stringify(Ripper.sexp(orig_code)) + "<p>" + stringify(treeify(Ripper.sexp(orig_code), "").flatten)
     when 'Compile'
-      @code = arrayify(RubyVM::InstructionSequence.compile(orig_code).disasm)
+      begin
+        @code = arrayify(RubyVM::InstructionSequence.compile(orig_code).disasm)
+      rescue SyntaxError => e
+        @code = ["Could not compile code - Syntax error"]
+      end
     end
   end
 
